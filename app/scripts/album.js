d47994792606083b5cb5751cc4a15e54878b6849
Carlos Velasquez <cv.velasquez@gmail.com>
2015-02-22 16:54:34 -0800

    Added functionality to player controls

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -114,9 +114,49 @@ var createSongRow = function(songNumber, songName, songLength) {
      var $newRow = createSongRow(i + 1, songData.name, songData.length);
      $songList.append($newRow);
    }
- 
  };
 
+var updateSeekPercentage =function($seekBar, event) {
+  var barWidth = $seekBar.width();
+  var offsetX = event.pageX - $seekBar.offset().left;
+
+  var offsetXPercent = (offsetX / barWidth) * 100;
+  offsetXPercent = Math.max(0, offsetXPercent);
+  offsetXPercent = Math.min(100, offsetXPercent);
+
+  var percentageString = offsetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+}
+
+var setupSeekBars = function() {
+
+  $seekBars = $('.player-bar .seek-bar');
+  $seekBars.click(function(event) {
+    updateSeekPercentage($(this), event);
+  });
+
+    $seekBars.find('thumb').mousedown(function(event){
+      var $seekBar = $(this).parent();
+
+        $seekBar.addClass('no-animate');
+
+      $(document).bind('mousemove.thumb', function(event){
+        updateSeekPercentage($seekBar, event);
+      });
+    
+
+    ///cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+
+    });
+
+};
  
  
  // This 'if' condition is used to prevent the jQuery modifications
@@ -125,7 +165,7 @@ var createSongRow = function(songNumber, songName, songLength) {
  if (document.URL.match(/\/album.html/)) {
    // Wait until the HTML is fully processed.
    $(document).ready(function() {
-      changeAlbumView(albumPicasso);
+    changeAlbumView(albumPicasso);
       setupSeekBars();
     });
   }
