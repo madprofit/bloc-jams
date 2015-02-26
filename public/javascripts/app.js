e2d3e302760fa3dcf25492cd58b2eed7e43d5e83
Carlos Velasquez <cv.velasquez@gmail.com>
2015-02-26 03:35:15 +0000

    Single page application checkpoint.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -271,24 +271,36 @@ var setupSeekBars = function() {
 //require('./album');
 //require('./profile');
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+blocJams = angular.module('BlocJams', ['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+}]);
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "do something!";
 
-    $scope.subTextClicked = function() {
+  $scope.subTextClicked = function() {
      $scope.subText += '!';
    };
 
-     $scope.albumURLs = [
-        '/images/album-placeholders/album-1.jpg',
-        '/images/album-placeholders/album-2.jpg',
-        '/images/album-placeholders/album-3.jpg',
-        '/images/album-placeholders/album-4.jpg',
-        '/images/album-placeholders/album-5.jpg',
-        '/images/album-placeholders/album-6.jpg',
-        '/images/album-placeholders/album-7.jpg',
-        '/images/album-placeholders/album-8.jpg',
-        '/images/album-placeholders/album-9.jpg',
-      ];
+  $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
 }]);
 });
 
