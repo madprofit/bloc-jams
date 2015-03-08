4c8cc45f1333783e1f26d91085c3b283e87ff3aa
Carlos Velasquez <cv.velasquez@gmail.com>
2015-03-08 05:37:15 +0000

    Built a SongPlayer service to share song data across views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -105,11 +105,11 @@ blocJams.controller('Album.controller', ['$scope', function($scope) {
       };
 
 //=======
-  blocJams.controller('Album.controller', ['$scope', function($scope) {
+  blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
     $scope.album = angular.copy(albumPicasso);
 
       var hoveredSong = null;
-      var playingSong = null;
+      
 
       $scope.onHoverSong = function(song) {
         hoveredSong = song;
@@ -121,7 +121,7 @@ blocJams.controller('Album.controller', ['$scope', function($scope) {
       }]);
 
         $scope.getSongState = function(song) {
-          if (song === playingSong) {
+          if (song === SongPlayer.currentSong && SongPlayer.playing) {
             return 'playing';
           }
           else if (song === hoveredSong) {
@@ -132,13 +132,37 @@ blocJams.controller('Album.controller', ['$scope', function($scope) {
 
 //>>>>>>> angular-album-view
         $scope.playSong = function(song) {
-          playingSong = song;
+          SongPlayer.setSong($scope.album, song);
+          SongPlayer.play();
         };
 
         $scope.pauseSong = function(song) {
-          playingSong = null;
+          SongPlayer.pause();
         };
           }]);
+
+  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+  $scope.songPlayer = SongPlayer;
+}]);
+
+    blocJams.service('SongPlayer', function() {
+      return {
+        currentSong: null,
+        currentAlbum: null,
+        playing: false,
+
+        play: function() {
+          this.playing = true;
+        },
+        pause: function() {
+          this.playing = false;
+        },
+        setSong: function(album, song) {
+          this.currentAlbum = album;
+          this.currentSong = song;
+        }
+      };
+    });
 //<<<<<<< HEAD
 
 //=======
